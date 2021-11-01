ac0c6791866c79a3c96e3040c5827b092d217e59
adebadayo <adebayanen@gmail.com>
2021-11-01 23:28:14 +0900

    useState

--- src/App.js
+++ src/App.js
@@ -1,27 +1,41 @@
 import React, { useState } from 'react'
 
-const LogoutButton = (props) => (
-  <button onClick={props.toggleIsLoggedIn}>ログアウト</button>
-)
-const LoginButton = (props) => (
-  <button onClick={props.toggleIsLoggedIn}>ログイン</button>
-)
-// const LoginButton
 
-const LoginControl = () => {
-  const [isLoggedIn, setIsLoggedInState] = useState(false)
+const INITIAL_COUNT = 0
+const INITIAL_NAME = ''
 
-  const toggleIsLoggedIn = () => {
-    setIsLoggedInState(!isLoggedIn)
+const SampleComponent = () => {
+  const [count, setCount] = useState(INITIAL_COUNT)
+
+  const [name, setName] = useState(INITIAL_NAME)
+
+  const countIncrement = () => setCount(prevCount =>  prevCount + 1 )
+  const countDecrement = () => setCount(prevCount =>  prevCount - 1 )
+  const resetCount = () => setCount(INITIAL_COUNT)
+
+  const handleChangeName = (e) => {
+    setName(e.target.value)
   }
 
-  if(isLoggedIn){
-    return <LoginButton toggleIsLoggedIn={toggleIsLoggedIn}/>
-  }
+  return (
+    <div>
+      <p>現在のカウント : <b>{ count }</b></p>
+      <p>countの初期値 : <b>{ INITIAL_COUNT }</b></p>
 
-  return <LogoutButton toggleIsLoggedIn={toggleIsLoggedIn}/>
+      <button onClick={countIncrement}>increment</button>
+      <button onClick={countDecrement}>decrement</button>
+      <button onClick={resetCount}>reset</button>
+      <br/>
+
+      <input type="text" onChange={handleChangeName}/>
+
+      <br/>
+      <p>{ name }</p>
+    </div>
+  )
+
 }
 
 export default function App(){
-  return <LoginControl />
+  return <SampleComponent />
 }
